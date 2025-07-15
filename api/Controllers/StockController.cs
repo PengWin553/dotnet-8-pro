7143e45d26c37148d88053ed30a1072afc7aba37
PengWin553 <victoriakobayashi553@gmail.com>
2025-07-16 02:55:58 +0800

    Async/Await: Refactor StockController.cs' methods
    await = "Start cooking this, I'll come back when it's ready!"
    
    When you order food that takes time to cook, you don't stand at the counter blocking other customers. Instead, you get a buzzer and sit down, allowing the cashier to serve others. Once your food is ready (the awaited operation completes), you're called back to pick it up (the method resumes from where it left off)

--- api/Controllers/StockController.cs
+++ api/Controllers/StockController.cs
@@ -7,6 +7,7 @@ using api.Data;
 using api.Models;
 using api.Mappers; 
 using api.Dto.Stock;
+using Microsoft.EntityFrameworkCore; // ToListAsync(), FirstOrDefaultAsync(), and FindAsync() are extension methods provided by Entity Framework Core
 
 namespace api.Controllers
 {
@@ -22,18 +23,18 @@ namespace api.Controllers
         }
 
         [HttpGet]
-        public IActionResult GetAll()
+        public async Task<IActionResult> GetAll()
         {
-            var stocks = _context.Stocks.ToList() // connected to the Stocks in your ApplicationDBContext
-             .Select(s => s.ToStockDto()); // return many (like js map)
+            var stocks = await _context.Stocks.ToListAsync(); // connected to the Stocks in your ApplicationDBContext
+            var stockDto = stocks.Select(s => s.ToStockDto()); // return many (like js map)
 
-            return Ok(stocks);
+            return Ok(stockDto);
         }
 
         [HttpGet("{id}")]
-        public IActionResult GetById([FromRoute] int id)
+        public async Task<IActionResult> GetById([FromRoute] int id)
         {
-            var stock = _context.Stocks.Find(id);
+            var stock = await _context.Stocks.FindAsync(id);
 
             if(stock == null)
             {
@@ -44,19 +45,19 @@ namespace api.Controllers
         }
 
         [HttpPost]
-        public IActionResult Create([FromBody] CreateStockRequestDto stockDto)
+        public async Task<IActionResult> Create([FromBody] CreateStockRequestDto stockDto)
         {
             var stockModel = stockDto.ToStockFromCreateDto();
-            _context.Stocks.Add(stockModel);
-            _context.SaveChanges();
+            await _context.Stocks.AddAsync(stockModel);
+            await _context.SaveChangesAsync();
             return CreatedAtAction(nameof(GetById), new { id = stockModel.Id}, stockModel.ToStockDto());
         }
 
         [HttpPut]
         [Route("{id}")]
-        public IActionResult Update([FromRoute] int id, [FromBody] UpdateStockRequestDto updateDto)
+        public async Task<IActionResult> Update([FromRoute] int id, [FromBody] UpdateStockRequestDto updateDto)
         {
-            var stockModel = _context.Stocks.FirstOrDefault(x => x.Id == id);
+            var stockModel = await _context.Stocks.FirstOrDefaultAsync(x => x.Id == id);
 
             if (stockModel == null)
             {
@@ -70,24 +71,24 @@ namespace api.Controllers
             stockModel.Industry = updateDto.Industry;
             stockModel.MarketCap = updateDto.MarketCap;
 
-            _context.SaveChanges();
+            await _context.SaveChangesAsync();
 
             return Ok(stockModel.ToStockDto());
         }
 
         [HttpDelete]
         [Route("{id}")]
-        public IActionResult Delete([FromRoute] int id)
+        public async Task<IActionResult> Delete([FromRoute] int id)
         {
-            var stockModel =  _context.Stocks.FirstOrDefault(x => x.Id == id);
+            var stockModel = await _context.Stocks.FirstOrDefaultAsync(x => x.Id == id);
 
             if (stockModel == null)
             {
                 return NotFound();
             }
 
-            _context.Stocks.Remove(stockModel);
-            _context.SaveChanges();
+            _context.Stocks.Remove(stockModel); // Remove is not an asynchronus function
+            await _context.SaveChangesAsync();
             return NoContent();
         }
     }
